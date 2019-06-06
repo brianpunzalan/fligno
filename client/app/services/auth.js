9a060ec1b021f5d134e41e7e37c317daf69b50c4
brianpunzalan <punzalan25brian@gmail.com>
2019-06-06 15:58:23 +0800

    update api login

--- client/app/services/auth.js
+++ client/app/services/auth.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 import { LOGIN } from 'constants/apiPaths';
 
-console.log(process.env.SERVER_URL);
-
 axios.defaults.baseURL = process.env.SERVER_URL;
 axios.defaults.headers.common = {
   Accept: 'application/json',
